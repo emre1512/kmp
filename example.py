e9896a9a8326cc54e2f5b54e592c41d41448b1ed
Emre <emre151291@gmail.com>
2020-04-26 10:38:43 +0300

    modified example

--- example.py
+++ example.py
@@ -1,12 +1,10 @@
 from KMP import KMP
 
 
-s = "ababab"
-pat1 = "bab"
-pat2 = "abc"
+s = "Hayalperestsin güzel hayaller peşinde, çok gençsin yanlış insanlar kalbinde"
+pat = "hayaller"
 
 kmp = KMP(s)
 
-print(kmp.kmp_table())  # [0, 0, 1, 2, 3, 4]
-print(kmp.kmp_search(pat1)) # 1
-print(kmp.kmp_search(pat2)) # None
+""" print(kmp.kmp_table()) """  # print kmp table
+print(kmp.kmp_search(pat)) # print index
